d3ea387edfa7bc9caa5e2994c58961b9606a8c31
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-03-01 17:09:21 -0500

    Edit page started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import CreatePotluck from './ComponentUnit3/CreatePotluck'
+import EditPotluck from './ComponentUnit3/EditPotluck'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <CreatePotluck  />
+      <Route exact path='/create' component={CreatePotluck}/>
+      <Route exact path='/edit' component={EditPotluck}/>
     </div>
   );
 }
