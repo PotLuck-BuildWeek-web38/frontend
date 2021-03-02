c62bda1ec37876d26c878887af45a37c5a49934e
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-01 19:41:28 -0500

    app.js has been changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import PotLuck from './ComponentUnit3/PotlLuck';
+import React from 'react'
+import CreatePotluck from './ComponentUnit3/CreatePotluck'
+import EditPotluck from './ComponentUnit3/EditPotluck'
+import {Route} from 'react-router-dom'
+import Login from './ComponentUnit2/Login';
+import PotLuck from './ComponentUnit3/PotlLuck'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <PotLuck />
-      </header>
+    <div>
+      <Route exact path='/login'/>
+      <Route exact path='/PotLuck' component={PotLuck}/>
+      <Route exact path='/create' component={CreatePotluck}/>
+      <Route exact path='/edit' component={EditPotluck}/>
+      
     </div>
   );
 }
