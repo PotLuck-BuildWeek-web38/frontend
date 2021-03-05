e88893e026e1ff0cfad9b85ff19fb312cac2967d
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-05 11:16:45 -0500

    styles added, guest list added, guest is being checked, need to work on potluckid

--- frontend/src/ComponentUnit3/guest.js
+++ frontend/src/ComponentUnit3/guest.js
@@ -1,16 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 import {axiosWithAuth} from '../utils/axiosWithAuth';
+import './css/guest.css'
 
 const GuestInfo = () => {
     const [guest, setGuest] = useState([]);
+    const [checked, setChecked] = useState({});
 
     useEffect(() => {
         axiosWithAuth()
             .get('/users/users')
             .then((response) => {
-                console.log("something")
-               //console.log('guest',response)
+               console.log(response)
                setGuest(response.data);
                console.log(guest)
            })
@@ -19,9 +20,50 @@ const GuestInfo = () => {
            })
     }, []);
 
+    const changeHandler = (event) => {
+        //console.log(checked)
+        setChecked({...checked, [event.target.name]:event.target.checked})
+        //console.log(checked)
+    }
+
+    useEffect(() => {
+        console.log("checked: ", checked);
+      }, [checked]); 
+
+    const handleSubmit = (event) => {
+        const {value} = event.target;
+        console.log(value);
+        axiosWithAuth()
+             .post(`potlucks/potluck/${value}/addguest/${value}`)
+             .then((response) => {
+                 console.log(response)
+             })
+             .catch(error => {
+                 console.error(error)
+             })
+    }
+   //potlucks/potluck/{potluckid}/addguest/{userid}
+
     return(
-        <div>
-            <h2>Guest List</h2>
+        <div className="gi">
+            <h2 className="h2">Invite Guest</h2>
+            <div class='guestInfo'>
+                <form onClick={handleSubmit}>
+                    {guest.map(guestInfo => (
+                            <div class='guest' key={guestInfo.userid}> 
+                                <p>Name: {guestInfo.username}</p>
+                                            <div className="emailCheckbox">
+                                                {guestInfo.primaryemail}
+                                                <input type="checkbox" checked={checked[guestInfo.primaryemail]} name={guestInfo.primaryemail} value={guestInfo.userid} onChange={changeHandler}/>
+                                                {/* <input type="checkbox" checked={guestInfo.primaryemail ? checked : ""} name="guestEmail" value={guestInfo.userid} onChange={changeHandler}/> */}
+                                            </div>                   
+                            </div>
+                    ))}
+                    <div className="button">
+                        <button>Invite Guest</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 
